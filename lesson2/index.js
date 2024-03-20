75e219c330e3345184967440804bed631ee757cf
Dmitriy Tolchkov <dmitrijj-tlchkv@rambler.ru>
2024-03-20 14:55:20 +0300

    second commit

--- lesson2/index.js
+++ lesson2/index.js
@@ -1,46 +1,46 @@
 /* const a = 1;
-let result = a < 0 ? "true" : "false";
+let result = a < 0 ? "верно" : "не верно";
 
 console.log(result);
  */
 /* const a = 0;
-let result = a < 0 ? "true" : "false";
+let result = a < 0 ? "верно" : "не верно";
 
 console.log(result); */
 
 /* const a = -30;
-let result = a < 0 ? "true" : "false";
+let result = a < 0 ? "верно" : "не верно";
 
 console.log(result); */
 
 /* const a = 1;
-let result = a >= 0 ? "true" : "false";
+let result = a >= 0 ? "верно" : "не верно";
 
 console.log(result);
  */
 /* const a = 0;
-let result = a >= 0 ? "true" : "false";
+let result = a >= 0 ? "верно" : "не верно";
 
 console.log(result); */
 
 /* const a = -30;
-let result = a >= 0 ? "true" : "false";
+let result = a >= 0 ? "верно" : "не верно";
 
 console.log(result);
  */
 /* const a = 1;
-let result = a <= 0 ? "true" : "false";
+let result = a <= 0 ? "верно" : "не верно";
 
 console.log(result);
  */
 
 /* const a = 0;
-let result = a <= 0 ? "true" : "false";
+let result = a <= 0 ? "верно" : "не верно";
 
 console.log(result); */
 
 /* const a = -30;
-let result = a <= 0 ? "true" : "false";
+let result = a <= 0 ? "верно" : "не верно";
 
 console.log(result); */
 
@@ -101,9 +101,9 @@ if (a <= 0) {
   console.log("не верно");
 } */
 
-const a = -30;
+/* const a = -30;
 if (a <= 0) {
   console.log("верно");
 } else if (a >= 0) {
   console.log("не верно");
-}
+} */
