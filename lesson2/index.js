f00262897afbf96ef7f7f5cb77571324b87539eb
Dmitriy Tolchkov <dmitrijj-tlchkv@rambler.ru>
2024-03-21 15:07:58 +0300

    second commit

--- lesson2/index.js
+++ lesson2/index.js
@@ -1,3 +1,5 @@
+/* Работа с if-else */
+
 /* const a = 1;
 let result = a == 0 ? "верно" : "не верно";
 
@@ -256,6 +258,8 @@ if (a == "1") {
   console.log("не верно");
 } Ответ не верно, убирается все лишнее "" и выполняется  сравнивание */
 
+/* Работа с логическими переменными */
+
 /* const test = true;
 let result = test == true ? "верно" : "не верно";
 
@@ -268,14 +272,99 @@ if (test == true) {
   console.log("не верно");
 } */
 
-/* const test = true;
-let result = test == false ? "верно" : "не верно";
+/* const test = false;
+let result = test == true ? "верно" : "не верно";
 
 console.log(result); */
 
-const test = false;
+/* const test = false;
 if (test == true) {
   console.log("верно");
 } else {
+  console.log("не верно"); */
+
+/* const test = true;
+let result = test != true ? "верно" : "не верно";
+
+console.log(result);  Ответ не верно, т.к переменная true */
+
+/* const test = true;
+if (test != true) {
+  console.log("верно");
+} else {
   console.log("не верно");
+} */
+
+/* const test = false;
+let result = test != true ? "верно" : "не верно";
+
+console.log(result); Ответ верно*/
+
+/* const test = false;
+if (test != true) {
+  console.log("верно");
+} else {
+  console.log("не верно");
+} */
+
+/* Работа с && (и) и || (или) */
+
+/* const a = 5;
+if (a > 0 && a < 5) {
+  console.log("верно");
+} else {
+  console.log("не верно");
+} Ответ не верно*/
+
+/* const a = 0;
+if (a > 0 && a < 5) {
+  console.log("верно");
+} else {
+  console.log("не верно");
+} Ответ не верно, т.к не выполняется первое условие if */
+
+/* const a = -3;
+if (a > 0 && a < 5) {
+  console.log("верно");
+} else {
+  console.log("не верно");
+} Ответ не верно, т.к не выполняется первое условие if */
+
+/* const a = 2;
+if (a > 0 && a < 5) {
+  console.log("верно");
+} else {
+  console.log("не верно");
+} Ответ верно, выполняется условие if */
+
+/* let a = 5;
+if (a == 0 || a == 2) {
+  a += 7;
+} else {
+  a /= 10;
 }
+console.log(a); Переменная = 0.5*, выполняется условие else */
+
+/* let a = 0;
+if (a == 0 || a == 2) {
+  a += 7;
+} else {
+  a /= 10;
+}
+console.log(a); Переменная = 7*/
+
+/* let a = -3;
+if (a == 0 || a == 2) {
+  a += 7;
+} else {
+  a /= 10;
+}
+console.log(a);  Переменная = -0,3, выполняется условие else */
+
+/* let a = 2;
+if (a == 0 || a == 2) {
+  a += 7;
+} else {
+  a /= 10;
+}
+console.log(a); Переменная = 9 */
