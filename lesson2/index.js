ebade584a39edb0698b0882b81f8bd2ba4b9e413
Dmitriy Tolchkov <dmitrijj-tlchkv@rambler.ru>
2024-03-21 10:55:30 +0300

    second commit

--- lesson2/index.js
+++ lesson2/index.js
@@ -1,139 +1,215 @@
 /* const a = 1;
 let result = a == 0 ? "верно" : "не верно";
 
-console.log(result); */ /*Ответ не верно*/
+console.log(result); Ответ не верно*/
+
+/* const a = 1;
+if (a == 0) {
+  console.log("верно");
+} else {
+  console.log("не верно");
+} Ответ не верно*/
 
 /* const a = 0;
 let result = a == 0 ? "верно" : "не верно";
 
-console.log(result); */ /*Ответ верно*/
+console.log(result); Ответ верно*/
+
+/* const a = 0;
+if (a == 0) {
+  console.log("верно");
+} else {
+  console.log("не верно");
+} Ответ верно*/
 
 /* const a = -3;
 let result = a == 0 ? "верно" : "не верно";
 
-console.log(result); */ /*Ответ не верно*/
+console.log(result); Ответ не верно*/
+
+/* const a = -3;
+if (a == 0) {
+  console.log("верно");
+} else {
+  console.log("не верно");
+} Ответ не верно*/
 
 /* const a = 1;
 let result = a > 0 ? "верно" : "не верно";
 
-console.log(result); */ /*Ответ верно*/
+console.log(result); Ответ верно*/
+
+/* const a = 1;
+if (a > 0) {
+  console.log("верно");
+} else {
+  console.log("не верно");
+} Ответ верно*/
 
 /* const a = 0;
 let result = a > 0 ? "верно" : "не верно";
 
-console.log(result);
- */ /*Ответ не верно*/
+console.log(result); Ответ не верно*/
+
+/* const a = 0;
+if (a > 0) {
+  console.log("верно");
+} else {
+  console.log("не верно");
+} Ответ не верно*/
 
 /* const a = -3;
 let result = a > 0 ? "верно" : "не верно";
 
-console.log(result); */ /*Ответ не верно*/
+console.log(result); Ответ не верно*/
+
+/* const a = -3;
+if (a > 0) {
+  console.log("верно");
+} else {
+  console.log("не верно");
+} Ответ не верно*/
 
 /* const a = 1;
 let result = a < 0 ? "верно" : "не верно";
 
-console.log(result);*/
+console.log(result); Ответ не верно*/
+
+/* const a = 1;
+if (a < 0) {
+  console.log("верно");
+} else {
+  console.log("не верно");
+} Ответ не верно*/
 
 /* const a = 0;
 let result = a < 0 ? "верно" : "не верно";
 
-console.log(result); */
+console.log(result); Ответ не верно*/
+
+/* const a = 0;
+if (a < 0) {
+  console.log("верно");
+} else {
+  console.log("не верно");
+} Ответ не верно*/
 
 /* const a = -3;
 let result = a < 0 ? "верно" : "не верно";
 
-console.log(result); */
-
-/* const a = 1;
-let result = a >= 0 ? "верно" : "не верно";
-
-console.log(result); */
-
-/* const a = 0;
-let result = a >= 0 ? "верно" : "не верно";
-
-console.log(result); */
-
-/* const a = -3;
-let result = a >= 0 ? "верно" : "не верно";
-
-console.log(result); */
-
-/* const a = 1;
-let result = a <= 0 ? "верно" : "не верно";
-
-console.log(result); */
-
-/* const a = 0;
-let result = a <= 0 ? "верно" : "не верно";
-
-console.log(result); */
-
-/* const a = -3;
-let result = a <= 0 ? "верно" : "не верно";
-
-console.log(result); */
-
-/* const a = 1;
-if (a < 0) {
-  console.log("верно");
-} else if (a > 0) {
-  console.log("не верно");
-}
- */
-
-/* const a = 0;
-if (a < 0) {
-  console.log("верно");
-} else if (a > 0) {
-  console.log("не верно");
-} */
+console.log(result); Ответ верно*/
 
 /* const a = -3;
 if (a < 0) {
   console.log("верно");
-} else if (a > 0) {
+} else {
   console.log("не верно");
-} */
+} Ответ верно*/
+
+/* const a = 1;
+let result = a >= 0 ? "верно" : "не верно";
+
+console.log(result); Ответ верно*/
 
 /* const a = 1;
 if (a >= 0) {
   console.log("верно");
-} else if (a <= 0) {
+} else {
   console.log("не верно");
-} */
+} Ответ верно*/
+
+/* const a = 0;
+let result = a >= 0 ? "верно" : "не верно";
+
+console.log(result); Ответ верно*/
 
 /* const a = 0;
 if (a >= 0) {
   console.log("верно");
-} else if (a <= 0) {
+} else {
   console.log("не верно");
-} */
+} Ответ верно*/
+
+/* const a = -3;
+let result = a >= 0 ? "верно" : "не верно";
+
+console.log(result); Ответ не верно*/
 
 /* const a = -3;
 if (a >= 0) {
   console.log("верно");
-} else if (a <= 0) {
+} else {
   console.log("не верно");
-} */
+} Ответ не верно*/
+
+/* const a = 1;
+let result = a <= 0 ? "верно" : "не верно";
+
+console.log(result); Ответ не верно*/
 
 /* const a = 1;
 if (a <= 0) {
   console.log("верно");
-} else if (a >= 0) {
+} else {
   console.log("не верно");
-} */
+} Ответ не верно*/
+
+/* const a = 0;
+let result = a <= 0 ? "верно" : "не верно";
+
+console.log(result); Ответ верно*/
 
 /* const a = 0;
 if (a <= 0) {
   console.log("верно");
-} else if (a >= 0) {
+} else {
   console.log("не верно");
-} */
+} Ответ верно*/
+
+/* const a = -3;
+let result = a <= 0 ? "верно" : "не верно";
+
+console.log(result); Ответ верно*/
 
 /* const a = -3;
 if (a <= 0) {
   console.log("верно");
-} else if (a >= 0) {
+} else {
   console.log("не верно");
-} */
+} Ответ верно*/
+
+/* const a = 0;
+let result = a != 0 ? "верно" : "не верно";
+
+console.log(result); Ответ не верно */
+
+/* const a = 0;
+if (a != 0) {
+  console.log("верно");
+} else {
+  console.log("не верно");
+} Ответ не верно*/
+
+/* const a = 1;
+let result = a != 0 ? "верно" : "не верно";
+
+console.log(result); Ответ верно*/
+
+/* const a = 1;
+if (a != 0) {
+  console.log("верно");
+} else {
+  console.log("не верно");
+} Ответ верно*/
+
+/* const a = -3;
+let result = a != 0 ? "верно" : "не верно";
+
+console.log(result); Ответ верно*/
+
+/* const a = -3;
+if (a != 0) {
+  console.log("верно");
+} else {
+  console.log("не верно");
+} Ответ верно*/
