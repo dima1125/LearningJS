5bca151170b92680b047b38febd7571d62553c3f
Dmitriy Tolchkov <dmitrijj-tlchkv@rambler.ru>
2024-03-21 12:17:03 +0300

    second commit

--- lesson2/index.js
+++ lesson2/index.js
@@ -213,3 +213,69 @@ if (a != 0) {
 } else {
   console.log("не верно");
 } Ответ верно*/
+
+/* const a = "test";
+if (a == "test") {
+  console.log("верно");
+} else {
+  console.log("не верно");
+} Ответ верно*/
+
+/* const a = "тест";
+if (a == "test") {
+  console.log("верно");
+} else {
+  console.log("не верно");
+}  Ответ не верно т.к слова "тест" не равен "test" */
+
+/* const a = 3;
+if (a == "test") {
+  console.log("верно");
+} else {
+  console.log("не верно");
+} Ответ не верно т.к число 3 не равен строке "test"*/
+
+/* const a = "1";
+if (a == "1") {
+  console.log("верно");
+} else {
+  console.log("не верно");
+} Ответ верно а это строка и "1" преобразуется в строку*/
+
+/* const a = 1;
+if (a == "1") {
+  console.log("верно");
+} else {
+  console.log("не верно");
+} Ответ верно, убирается все лишнее "" и выполняется  сравнивание*/
+
+/* const a = 3;
+if (a == "1") {
+  console.log("верно");
+} else {
+  console.log("не верно");
+} Ответ не верно, убирается все лишнее "" и выполняется  сравнивание */
+
+/* const test = true;
+let result = test == true ? "верно" : "не верно";
+
+console.log(result); Ответ верно, т.к переменная true*/
+
+/* const test = true;
+if (test == true) {
+  console.log("верно");
+} else {
+  console.log("не верно");
+} */
+
+/* const test = true;
+let result = test == false ? "верно" : "не верно";
+
+console.log(result); */
+
+const test = false;
+if (test == true) {
+  console.log("верно");
+} else {
+  console.log("не верно");
+}
