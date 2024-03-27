1febb6396ca1c1200f19c883151ab92f4336d00b
Dmitriy Tolchkov <dmitrijj-tlchkv@rambler.ru>
2024-03-27 15:57:29 +0300

    second commit

---
+++ lesson3/index.js
@@ -0,0 +1,60 @@
+/* function number(num) {
+  return num ** num;
+}
+console.log(number(5)); */
+
+/* function sum(a, b) {
+  return a + b;
+}
+const resultSum = sum(5, 9);
+console.log(resultSum); */
+
+/* function sub(a, b, c) {
+  return (a - b) / c;
+}
+const resultSub = sub(50, 10, 2);
+console.log(resultSub); */
+
+/* const day = 5;
+function dayWeek(day) {
+  switch (day) {
+    case 1:
+      console.log("понедельник");
+
+      break;
+    case 2:
+      console.log("вторник");
+
+      break;
+    case 3:
+      console.log("среда");
+
+      break;
+    case 4:
+      console.log("четверг");
+
+      break;
+    case 5:
+      console.log("пятница");
+
+      break;
+    case 6:
+      console.log("суббота");
+
+      break;
+    case 7:
+      console.log("воскресенье");
+  }
+  return day;
+}
+dayWeek(day);
+ */
+
+/* function num(num1, num2) {
+  return num1 == num2;
+}
+if (num(8, 13)) {
+  console.log("верно");
+} else {
+  console.log("не верно");
+} */
