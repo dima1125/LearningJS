86dc971687615e7ef5ff6dc9e5d6dbad968a3c54
Dmitriy Tolchkov <dmitrijj-tlchkv@rambler.ru>
2024-04-11 13:33:30 +0300

    second commit

--- lesson5/index.js
+++ lesson5/index.js
@@ -1,3 +1,5 @@
+//1. Используя метод map() напишите код, который получает из массива строк новый массив, содержащий их длины.
+
 /* let arr = ["Капуста", "Репа", "Редиска", "Морковка"];
 
 let newArr = arr.map(function (piece) {
@@ -5,6 +7,8 @@ let newArr = arr.map(function (piece) {
 });
 
 console.log(newArr); */
+
+//2. Имеется массив простых чисел: numbers = [2, 3, 5, 7, 11, 13, 17, 19]. Используя метод reduce() напишите функцию currentSums(numbers), которая возвращает новый массив из такого же числа элементов, в котором на каждой позиции будет находиться сумма элементов массива numbers до этой позиции включительно.
 
 /* const numbers = [2, 3, 5, 7, 11, 13, 17, 19];
 function currentSums(numbers) {
@@ -17,6 +21,8 @@ function currentSums(numbers) {
 }
 
 currentSums(numbers); */
+
+//3.Напишите код, который получает из массива чисел новый массив, содержащий пары чисел, которые в сумме должны быть равны семи: (0:7), (1:6) и т.д.
 
 /* const numbers = [0, 1, 2, 3, 4, 5, 6, 7];
 
@@ -47,6 +53,8 @@ function sumSeven(numbers) {
 console.log(sumSeven(numbers));
  */
 
+//4.Напишите код, создающий массив, который будет состоять из первых букв слов строки str.
+
 /* const str = "Каждый охотник желает знать, где сидит фазан";
 function firstChar(value, index, arr) {
   if (index == 0) return true;
@@ -56,12 +64,16 @@ let array = [].filter.call(str, firstChar);
 
 console.log(array); */
 
+//5.Напишите код, создающий массив, который будет состоять из строк, состоящих из предыдущего, текущего и следующего символа строки str.
+
 /* const word = "фазан";
 function getChars(value, index, str) {
   return str.substring(index - 1, index + 2);
 }
 let arr = Array.prototype.map.call(word, getChars);
 console.log(arr); */
+
+//6.Напишите код, преобразующий массив цифр, которые располагаются неупорядоченно, в массив цифр расположенных по убыванию их значений.
 
 /* let numerics = [3, 5, 7, 2, 9, 1, 8];
 function matching(first, second) {
@@ -73,18 +85,22 @@ function matching(first, second) {
 numerics = numerics.sort(matching);
 console.log(numerics); */
 
+//7.Напишите код, объединяющий три массива цифр, и располагающий цифры, в полученном массиве, в порядке убывания их значений через пробел.
+
 /* let a = [1, 2, 3];
 let b = [4, 5, 6];
 let c = [7, 8, 9];
 function getArr(a, b, c) {
+  getArr = [];
   let d = a.concat(b, c);
-  let f = d.pop();
-  while (f != undefined) {
-    console.log(f + " ");
-    f = d.pop();
+  {
+    let result = d.sort((a, b) => b - a);
+    console.log(result);
   }
 }
 getArr(a, b, c); */
+
+//8.Дан двухмерный массив с числами, например [[1, 2, 3], [4, 5], [6]]. Найдите сумму элементов этого массива. Массив, конечно же, может быть произвольным.
 
 /* let matrix = [
   [1, 2, 3],
@@ -98,9 +114,13 @@ let result = matrixSum.reduce((accumulator, current) => accumulator + current);
 
 console.log(result); */
 
+//9.Дан массив с числами. Не используя метода reverse переверните его элементы в обратном порядке.
+
 /* let arr = [0, 1, 2, 3, 4, 5, 6, 7];
 arr.sort((a, b) => b - a);
 console.log(arr); */
+
+//10.Дан массив с числами. Узнайте сколько элементов с начала массива надо сложить, чтобы в сумме получилось больше 10-ти.
 
 /* let arr = [0, 1, 2, 3, 4, 5, 6, 7];
 index = arr.find(
@@ -111,6 +131,17 @@ index = arr.find(
 );
 console.log(index + 1); */
 
-let arr = Array(4);
-arr.fill({ name: "Dima", Job: "notarialnay contora" });
-console.log(arr);
+//11.Напишите функцию arrayFill, которая будет заполнять массив заданными значениями. Первым параметром функция принимает значение, которым заполнять массив, а вторым - сколько элементов должно быть в массиве.
+
+/* let arr = Array.from(new Array(4), () => ({ name: "имя", position: null }));
+arr[0].name = "Dima";
+console.log(arr); */
+
+/* function arrayFill(x, count) {
+  let arr = [];
+  for (let i = 1; i <= count; i++) {
+    arr.push(x);
+  }
+  return arr;
+}
+console.log(arrayFill("x", 5)); */
