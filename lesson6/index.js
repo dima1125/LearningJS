b2386e49c274bcce08af69fbefc7c9bd90908914
Dmitriy Tolchkov <dmitrijj-tlchkv@rambler.ru>
2024-04-19 15:13:12 +0300

    second commit

--- lesson6/index.js
+++ lesson6/index.js
@@ -105,3 +105,25 @@ newFunc("Иванов, Иван", "Петров, Петр"); */
 };
 
 console.log(curry(2)(10)(-13)); */
+
+// 8. Реализовать таймер-функцию используя замыкания. Функция принимает два  аргумента начальное значение и значение завершения. Таймер движется назад.При достижении точки завершения в консоль выводится значение таймера и сообщение о завершении работы таймера.
+
+/* let timer;
+let n = 10;
+countdown();
+function countdown() {
+  document.getElementById("timer").innerHTML = x;
+  x--;
+  if (x < 1) {
+    clearTimeout(timer);
+    console.log("завершение работы таймера");
+  } else {
+    timer = setTimeout(countdown, 1000);
+  }
+} */
+
+/* const timerId = setTimeout(() => {
+  console.log("завершение работы таймера");
+}, 1000);
+
+console.log(timerId); */
